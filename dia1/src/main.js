28ea09f13efc9f38aef702334061da2aa8bd34d2
Alexandre A Barbosa <alexandre1202@gmail.com>
2023-10-23 13:21:21 -0300

    feat: exercicio 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,19 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const elementApp = document.querySelector('[data-js="app"]')
+elementApp.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector('[data-js="link"]')
+link.addEventListener('click', (e) => {
+  e.preventDefault()
+  elementApp.classList.toggle('hide')
+  e.target.textContent = elementApp.classList.contains('hide')
+    ? 'Exibir'
+    : 'Esconder'
+  // or...:
+  // const display = elementApp.style.display
+  // elementApp.style.display = display === 'none' ? 'block' : 'none'
+  // console.log('display', elementApp.style.display)
+})
